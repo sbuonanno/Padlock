729d0fe1875fadc169951a959d21f8bf5e7f9053
Sergio B <s.buonanno@kfi.it>
2023-03-24 16:40:47 +0100

    Initial commit

---
+++ src/main/java/it/kfi/padlock/PadlockApplication.java
@@ -0,0 +1,13 @@
+package it.kfi.padlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadlockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/kfi/padlock/PadlockApplicationTests.java
@@ -0,0 +1,13 @@
+package it.kfi.padlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
